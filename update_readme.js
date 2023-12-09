e0e1c9c110dcf92f69475432cb306fe192755f19
SuhravHussen <suhravshan@gmail.com>
2023-12-09 13:37:42 +0600

    Initial setup with templates and workflow

--- update_readme.js
+++ update_readme.js
@@ -13,55 +13,37 @@ const weatherData = [
 ];
 
 const template = `
-<!DOCTYPE html>
+<h1 align="center"><b>Hi , I'm Suhrav Hussen </b><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35"></h1>
+<!--  -->
+<p align="center">
+  <a href="https://github.com/DenverCoder1/readme-typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=cyan&size=25&center=true&vCenter=true&width=600&height=100&lines=Assalamu+O+Alaikum+Warahmatullah..&hearts;++;I'm a self-taught+developer;++;I'm from  from moulvibazar, sylhet "></a>
+</p>
+<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"><br><br>
+<!--  -->
 
-<html
- 
-lang="en">
+<h3>My resent articles</h3>
+<table>
+{{#articles}}
+        <tr>
+            <td width="300px">
+                <a href="https://suhravhussen.xyz/blogs/{{id}}">
+                    <img src={{image}} alt="thumbnail">
+                </a>
+            </td>
+            <td>
+            <a href="https://suhravhussen.xyz/blogs/{{id}}">{{title}}</a>
+                <div>{{text}}</div>
+                <div><i>26/10/2023</i></div>
+            </td>
+        </tr>
+        {{/articles}}
+</table>
 
-<head>
+<div align="right">
 
-  
-<meta
- 
-charset="UTF-8">
+*Updated at: 2023-12-09T06:27:54Z - by **[Suhrav Hussen](https://suhravhussen.xyz)***
+</div>
 
-  
-<meta
- 
-name="viewport"
- 
-content="width=device-width, initial-scale=1.0">
-
-  
-<title>{{title}}</title>
-
-</head>
-
-<body>
-  <header>
-    <img src="{{cover_image}}" alt="{{title}}">
-    <h1>{{title}}</h1>
-    <p>{{description}}</p>
-  </header>
-
-  <main>
-    ## Articles
-
-    {{#articles}}
-      <article>
-        <h2>{{title}}</h2>
-        <p>By Suhrav Hussen on 2023</p>
-        <img src="{{image}}" alt="{{title}}">
-      
-      </article>
-    {{/articles}}
-
-  </main>
-
-
-</body>
-</html>
 `;
 
 const renderedReadme = Mustache.render(template, {
